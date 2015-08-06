7a705e3639d2f77ac8e66557753b3fc2a5f34692
comboyin <comboyin@comboyin-K72Jr>
2015-08-07 03:23:28 +0700

    building NhatKy ...

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<script type="text/javascript" src="js_css/jquery.js"></script>
+	<script type="text/javascript" src="js_css/jquery-1.8.3.min.js"></script>
 	<script type="text/javascript" src="js_css/jquery.yiiactiveform.js"></script>
+	<script type="text/javascript" src="js_css/jquery.dataTables.js"></script>
+	<script type="text/javascript" src="js_css/DT_bootstrap.js"></script>
 	<title>TRA CỨU TỜ KHAI THUẾ PNN</title>
 	<link href="js_css/reset.css" rel="stylesheet" type="text/css">
 	<link href="js_css/general.css" rel="stylesheet" type="text/css">
@@ -38,142 +40,24 @@
 				<p>Thứ năm, 30 - 07 - 2015,  11:15, GMT+7</p>
 				<p class="welcome" style="text-transform: uppercase">CHI CỤC THUẾ QUẬN 5 - ĐỘI THUẾ LIÊN PHƯỜNG 5</p>
 			</div>
-		</div>
-aa
+			
+		</div>		
         <!-- header.end -->
+        -
+        
         <!-- main -->
         <div id="main">
             <div id="main-content-web">
-                <div class="clear-fix" id="checkSignIn" style="min-height: 275px;">
-				<div class="group-1" id="personelDetail"><h1 class="title">Lý Lịch Tóm Lược của Nhân viên</h1>
-				<div class="content">
-					<div class="pic"><img style="width: 120px;" src="/uploads/users/469/chu.tranvan@stu.edu.vn_1429084377.png" alt="Nhân viên"></div>
-					<div class="info">
-						<ul class="list">
-							<li class="type-1">Họ và tên: <span>Trần Văn Chử</span></li>
-							<li class="type-5">Giới tính: <span>Nam</span></li>
-											<li>Ngày sinh: <span>07 -05 - 1961</span></li>
-										<li>Học hàm: <span></span></li>
-											<li>Quê quán: <span>Thái Bình</span></li>
-														<li>Dân tộc: <span>kinh</span></li>
-										<li>Chức vụ: <span>Nhân viên</span></li>                    
-							<li>Học vị: <span>Trung học chuyên nghiệp</span></li>
-											<li>Trình độ ngoại ngữ: <span></span></li>
-									</ul>
-						<p class="view-detail"><a href="http://personnel.stu.edu.vn/vi/xem-chi-tiet/469/chu.tranvan@stu.edu.vn.html">Xem chi tiết</a></p>
-					</div>
-				</div>
-				<p class="update-info"><a href="#">Cập nhật thông tin mới!</a></p>
-			</div>
-<div class="group-2">
-    <p class="title"><strong>Đăng nhập</strong></p>
-    <form id="login-form" action="/vi/chi-tiet/469/chu.tranvan@stu.edu.vn.html" method="post">        
-    <fieldset class="form">
-        <div id="login-form_es_" class="errorSummary" style="display:none"><p>Xin hãy sửa lại những lỗi nhập liệu sau:</p>
-<ul><li>dummy</li></ul></div>        <div class="input-1">                        
-            <label for="LoginForm_username" class="required">Tài Khoản <span class="required">*</span></label>            <input class="text-1" name="LoginForm[username]" id="LoginForm_username" type="text">            <div id="LoginForm_username_em_" class="errorMessage" style="display:none"></div>        </div>
-        <div class="input-1">
-            <label for="LoginForm_password" class="required">Mật Khẩu <span class="required">*</span></label>            <input class="text-1" name="LoginForm[password]" id="LoginForm_password" type="password">            <div id="LoginForm_password_em_" class="errorMessage" style="display:none"></div>        </div>
-        <p class="note"><em>(Đăng nhập để xem chi tiết)</em></p>
-        <div class="output" style="padding-left: 65px;">
-            <input type="submit" value="Đăng nhập" class="btn">
-        </div>
-    </fieldset>
-    <div class="link">
-        <span class="new"><a href="http://personnel.stu.edu.vn/vi/forgotPassword#content">Quên mật khẩu</a></span> 
-        <span class="all"><a href="http://personnel.stu.edu.vn/vi/active#content">Kích hoạt tài khoản</a></span>
-    </div>
-    </form></div></div>
-<div class="hr-list">
-    <p class="title"><strong>Hồ sơ pháp lý</strong></p><a name="listPerson"></a>
-    <div class="content clear-fix" id="listPersonItems" style="height: 271px;">
-        <!-- Loading box. Start -->
- 
-		<div id="loadingObj1439114157" style="float: left; text-align: center; width: 980px; border: 1px solid #C6EAFA; height: 175px;">
-			<ul class="clear-fix" style="width: 900px;" id="otherPersonelSubList">		
-				<li>
-					<a href="#id=461" alt="vu.phamhuy@stu.edu.vn" title="Phạm Huy Vũ">
-						<strong>KSCĐ. Phạm Huy Vũ</strong>
-						<img style="width:129px; height:162px;" src="/themes/classic/images/pattern-2.png" alt="Phạm Huy Vũ">
-						<span>Nhân viên</span>
-						(Phòng Công Tác Sinh Viên)
-					</a>
-				</li>          
-			
-				<li>
-					<a href="#id=454" alt="son.nguyenthanh@stu.edu.vn" title="Nguyễn Thanh Sơn">
-						<strong>KS. Nguyễn Thanh Sơn</strong>
-						<img style="width:129px; height:162px;" src="/uploads/users/454/son.nguyenthanh@stu.edu.vn_1418263650.png" alt="Nguyễn Thanh Sơn">
-						<span>Nhân viên</span>
-						(Phòng Quản Lý Khoa Học - Đối Ngoại)
-					</a>
-				</li>          
-			
-				<li>
-					<a href="#id=453" alt="dao.truongthianh@stu.edu.vn" title="Trương Thị Anh Đào">
-						<strong>TS. Trương Thị Anh Đào</strong>
-						<img style="width:129px; height:162px;" src="/uploads/users/453/dao.truongthianh@stu.edu.vn_1418264696.png" alt="Trương Thị Anh Đào">
-						<span>Giảng viên</span>
-						(Ban Khoa Học Cơ Bản)
-					</a>
-				</li>          
-			
-				<li>
-					<a href="#id=448" alt="van.haucong@stu.edu.vn" title="Hầu Công Văn">
-						<strong>Hầu Công Văn</strong>
-						<img style="width:129px; height:162px;" src="/themes/classic/images/pattern-2.png" alt="Hầu Công Văn">
-						<span>Nhân viên</span>
-						(Phòng Đào Tạo)
-					</a>
-				</li>          
-			
-				<li>
-					<a href="#id=447" alt="trung.phamlenhat@stu.edu.vn" title="Phạm Lê Nhật Trung">
-						<strong>Phạm Lê Nhật Trung</strong>
-						<img style="width:129px; height:162px;" src="/uploads/users/447/trung.phamlenhat@stu.edu.vn_1418263897.png" alt="Phạm Lê Nhật Trung">
-						<span>Nhân viên</span>
-						(Phòng Công Tác Sinh Viên)
-					</a>
-				</li>          											
-			</ul>
+                    <?php include 'source/router.php';?>
             
-			<div class="pager">
-				<span id="yw0" class="yiiPager">
-					<a class="type" href="#page=1"><img src="images/ico-arrow-4.gif" alt="tới"></a>
-					<strong>1</strong>
-					<a href="#page=2">2</a>
-					<a href="#page=3">3</a>
-					<a href="#page=4">4</a>
-					<a href="#page=5">5</a>
-					<a href="#page=6">6</a>
-					<a href="#page=7">7</a>
-					<a href="#page=8">8</a>
-					<a href="#page=9">9</a>
-					<a href="#page=10">10</a>
-					<a class="type" href="#page=2"><img src="images/ico-arrow-3.gif" alt="trước"></a>
-				</span>            
-			</div>
-          
-		</div>    
-
-	</div>
-</div>
-
-<!--Nhan su moi-->    
-<div class="jcarousel-next jcarousel-next-horizontal" disabled="false" style="display: block;"></div>
-
-</div>
             </div>
+        </div>
             <iframe frameborder="0" src="http://personnel.stu.edu.vn/tim-kiem.html" style="width:100%; height:1px" id="iframeGSearch" __idm_frm__="4">
                 [Your user agent does not support frames or is currently configured  not to display frames. However, you may visit the related document.]
             </iframe>
         </div>
 <!-- main.end -->
 <!-- footer -->
-<div id="footer">
-    <p class="link"><a href="http://stu.edu.vn/" class="first">Trang chủ</a><a href="http://personnel.stu.edu.vn/">Trang chủ nhân sự</a><a href="http://mail.stu.edu.vn">Email</a></p>    <p>© 2011 <strong>Trường Đại học Công Nghệ Sài Gòn</strong>. Liên Hệ: </p>
-    <p>180 Cao Lỗ, Phường 4, Quận 8, TP.HCM. ĐT: (84.8) 3850 5520. Fax: (84.8) 3850 6595.</p>
-    <p>Phòng Quản Lý Khoa Học - Đối Ngoại thiết kế và thực hiện</p>
-</div>
+<?php include_once 'source/footer.php';?>
 
 </body></html>
